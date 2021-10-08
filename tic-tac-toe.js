f047eca9f4389127693fc44ab189c29b3f74e1fb
TiaDavis <91109307+TiaDavis@users.noreply.github.com>
2021-10-08 16:37:54 -0400

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,7 @@
-
+window.addEventListener('load',function(){
+    var xOro=false;
+    var xcount=0;
+    var ocount=0;
+    var showboard= document.querySelectorAll('body #board div');
+    for(let i=0;i<showboard.length;i++){
+        showboard[i].classList.add('square');
